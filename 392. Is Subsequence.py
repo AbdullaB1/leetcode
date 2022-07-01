845fe1bc80fb88a6e95c9e860909e583037117d4
abegimkulov <abegimkulov@expertsolutions.ru>
2022-07-01 20:10:03 +0300

    modigyed 392. Is Subsequence.py

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -1,15 +1,15 @@
 class Solution:
-    def isSubsequence(self, source: str, target: str) -> bool:
-        s = 0
-        for t in target:
-            if s == len(source):
+    def isSubsequence(self, sub: str, big: str) -> bool:
+        idx = 0
+        for c in big:
+            if idx == len(sub):
                 return True
-            if t == source[s]:
-                s += 1
-        return s == len(source)
+            if c == sub[idx]:
+                idx += 1
+        return idx == len(sub)
 
 
 s = Solution()
 print(
-    s.isSubsequence("abc", "adfvsdfvsdfvabadcasvfdvfdvc")
+    s.isSubsequence("abc", "ahbgdc")
 )
