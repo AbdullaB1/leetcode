b4c351badc32009f6fd166d636a7e100084892db
abegimkulov <abegimkulov@expertsolutions.ru>
2022-07-02 18:18:49 +0300

    is_array_monotonic

---
+++ is_array_monotonic.py
@@ -0,0 +1,38 @@
+def is_monotonic(nums: list[int]) -> bool:
+    if len(nums) <= 2:
+        return True
+
+    if nums[0] == nums[-1]:
+        curr = nums[0]
+        for num in nums:
+            if num != curr:
+                return False
+        return True
+
+    if nums[0] < nums[-1]:
+        prev = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] < prev:
+                return False
+            prev = nums[i]
+        return True
+
+    if nums[0] > nums[-1]:
+        prev = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > prev:
+                return False
+            prev = nums[i]
+        return True
+
+
+assert is_monotonic([]) == True
+assert is_monotonic([-1]) == True
+assert is_monotonic([-1, 2]) == True
+assert is_monotonic([1, -22]) == True
+assert is_monotonic([1, 2, 2, 3]) == True
+assert is_monotonic([6, 5, 4, 4]) == True
+assert is_monotonic([1, 3, 2]) == False
+assert is_monotonic([-2, -1, 2, 3]) == True
+assert is_monotonic([-2, -1, -2, 3]) == False
+assert is_monotonic([3, 2, 2, -3]) == True
