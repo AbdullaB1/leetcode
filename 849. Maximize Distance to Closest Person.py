da479d390c012164463d41735c191223637641f3
abegimkulov <abegimkulov@expertsolutions.ru>
2022-07-03 13:18:46 +0300

    another solution for 849. Maximize Distance to Closest Person

--- 849. Maximize Distance to Closest Person.py
+++ 849. Maximize Distance to Closest Person.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_1:
     def maxDistToClosest(self, seats: list[int]) -> int:
         isfistseq = True
         result = 0
@@ -12,4 +12,25 @@ class Solution:
                     result = max(result, (i - start) // 2)
                 start = i
         result = max(result, len(seats) - start - 1)
+        return result
+
+
+class Solution:
+    """немного другая реализация, но идея та же"""
+
+    def maxDistToClosest(self, seats: list[int]) -> int:
+        result = 0
+        is_begin = True
+        curr = 0
+        for num in seats:
+            if num == 0:
+                curr += 1
+                continue
+            if is_begin:
+                result = curr
+                is_begin = False
+            else:
+                result = max(result, (curr - 1) // 2 + 1)
+            curr = 0
+        result = max(result, curr)
         return result
