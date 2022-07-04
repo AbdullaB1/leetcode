7db3de86a6611a53555628295d259ac016aaeefb
abegimkulov <abegimkulov@expertsolutions.ru>
2022-07-05 00:08:17 +0300

    added solution for multiple spaces between words - 557. Reverse Words in a String III

--- 557. Reverse Words in a String III.py
+++ 557. Reverse Words in a String III.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 from typing import List
 
 
@@ -29,3 +30,32 @@ class Solution:
             right -= 1
 
         return s
+
+
+class Solution:
+    """если бы пробелов могло быть неограниченное число"""
+    def reverseWords(self, s: str) -> str:
+        if not s:
+            return s
+        str_list = list(s)
+        prev = ' '
+        for i, c in enumerate(str_list):
+            # если будут не только пробелы, можно использовать не c.isspace() 
+            if prev == ' ' and c != ' ':
+                start = i
+            elif prev != ' ' and c == ' ':
+                self.reverse_one_word(str_list, start, i - 1)
+            prev = c
+        if str_list[-1] != ' ':
+            self.reverse_one_word(str_list, start, len(str_list) - 1)
+        return ''.join(str_list)
+
+    def reverse_one_word(self, s: List[str], left: int, right: int) -> None:
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+print(Solution().reverse_words(
+    "   Let's\t\t\ttake   LeetCode contest          ",
+), "|")
