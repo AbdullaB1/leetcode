2fb85813effb11f3b6494db55380302f6036a7c6
abegimkulov <abegimkulov@expertsolutions.ru>
2022-07-06 12:36:30 +0300

    added some another solution for 239. Sliding Window Maximum.py

--- 239. Sliding Window Maximum.py
+++ 239. Sliding Window Maximum.py
@@ -19,6 +19,22 @@ class Solution:
         return result
 
 
+class Solution:
+    """похожее решение, но для удаления из дека пользуемся индексами в массиве"""
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        maxes = deque()
+        result = []
+        for i, num in enumerate(nums):
+            while maxes and num > maxes[-1][1]:
+                maxes.pop()
+            maxes.append([i, num])
+            if i >= k - 1:
+                result.append(maxes[0][1])
+            if i >= k - 1 and maxes[0][0] <= i - k + 1:
+                maxes.popleft()
+        return result
+
+
 s = Solution()
 print(
     s.maxSlidingWindow(
