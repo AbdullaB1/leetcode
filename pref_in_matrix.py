a415ee4f1fbfa254e2fcb071497fd7ad81746784
abegimkulov <abegimkulov@expertsolutions.ru>
2022-07-11 17:50:04 +0300

    refactored pref_in_matrix.py

--- pref_in_matrix.py
+++ pref_in_matrix.py
@@ -1,12 +1,11 @@
 class PrefMatrix:
     def __init__(self, matrix: list[list[int]]) -> None:
-        self.matrix = matrix
-        self.pref = matrix
-        self.calculate_pref()
+        self.pref: list[list[int]] = matrix
+        self.calculate_pref_2()
 
     def calculate_pref(self) -> None:
-        rows = len(self.matrix)
-        cols = len(self.matrix[0])
+        rows = len(self.pref)
+        cols = len(self.pref[0])
         for i in range(0, rows):
             for j in range(1, cols):
                 self.pref[i][j] += self.pref[i][j - 1]
@@ -16,49 +15,35 @@ class PrefMatrix:
                 self.pref[i][j] += self.pref[i - 1][j]
         print(*self.pref, sep='\n')
 
+    # рассчет префиксной суммы за один проход по матрице
+    def calculate_pref_2(self) -> None:
+        rows = len(self.pref)
+        cols = len(self.pref[0])
+        for i in range(rows):
+            row_sum = 0
+            for j in range(cols):
+                row_sum += self.pref[i][j]
+                self.pref[i][j] = row_sum
+                self.pref[i][j] += self.pref[i - 1][j] if i - 1 >= 0 else 0
+        
+
     def query(self, x1: int, y1: int, x2: int, y2: int) -> int:
         upper = (self.pref[x1 - 1][y2] if x1 - 1 >= 0 else 0)
         left = (self.pref[x2][y1 - 1] if y1 - 1 >= 0 else 0)
         upper_left_corner = (
-            self.pref[x1 - 1][y1 - 1] if 0 <= x1 and 0 <= y1 else 0)
+            self.pref[x1 - 1][y1 - 1] if (0 <= x1 - 1 and 0 <= y1 - 1) else 0)
         print(self.pref[x2][y2], upper, left, upper_left_corner)
         return self.pref[x2][y2] - upper - left + upper_left_corner
 
 
 prefmatrix = PrefMatrix(
-    [
-        [1, -1, 3],
-        [4, 9, 10],
-        [1, 2, 3]
-    ]
+    # [
+    #     [1, -1, 3],
+    #     [4, 9, 10],
+    #     [1, 2, 3]
+    # ]
+    [[1, 2, 3]]
 )
 
-print(prefmatrix.query(1, 1, 2, 2))
-
-
-class PrefMatrix:
-    def __init__(self, matrix: list[list[int]]) -> None:
-        # создание матрицы
-        self.mas = []
-        for i in range(len(matrix)):
-            self.mas.append([0] * len(matrix[0]))
-
-        # пройдем по всем точкам этой матрицы
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-
-                # сложим все строчки
-                sub = 0
-                for k in range(i + 1):
-                    sub += sum(matrix[k][0:j + 1])
-                self.mas[i][j] = sub
-        print(*self.mas)
-
-
-prefmatrix = PrefMatrix(
-    [
-        [1, -1, 3],
-        [4, 9, 10],
-        [1, 2, 3]
-    ]
-)
+# print(prefmatrix.query(1, 1, 2, 2))
+print(prefmatrix.query(0, 1, 0, 2))
